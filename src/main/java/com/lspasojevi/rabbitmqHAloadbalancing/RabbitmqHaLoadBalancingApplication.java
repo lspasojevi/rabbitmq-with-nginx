d250cdcf43d8d75f3f3349816f8d30e65220921a
lspasojevi <lazar.spasojevic21@gmail.com>
2024-03-31 19:55:13 +0200

    Example of load balancing with nginx and rabbitmq
    first squash commit
    
    third commit squash
    
    fourth commit squash

---
+++ src/main/java/com/lspasojevi/rabbitmqHAloadbalancing/RabbitmqHaLoadBalancingApplication.java
@@ -0,0 +1,13 @@
+package com.lspasojevi.rabbitmqHAloadbalancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqHaLoadBalancingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqHaLoadBalancingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lspasojevi/rabbitmqHAloadbalancing/RabbitmqHaLoadBalancingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lspasojevi.rabbitmqHAloadbalancing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqHaLoadBalancingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
